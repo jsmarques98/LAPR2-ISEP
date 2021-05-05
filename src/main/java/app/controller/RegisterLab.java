f3c7b260f0a8cca43eb54cca60fdf90d64998f14
Joao Marques <1192221@isep.ipp.pt>
2021-05-05 11:24:04 +0100

    starting java us8

---
+++ src/main/java/app/controller/RegisterLab.java
@@ -0,0 +1,11 @@
+package app.controller;
+
+public class RegisterLab {
+
+    private String labId;
+    private String address;
+    private String phoneNr;
+    private String TINnr;
+    private String testName;
+    private String sample;
+}

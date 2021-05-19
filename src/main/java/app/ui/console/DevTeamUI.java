bc2c33a0bbb9d17b58c3bdac3a9caefbaea860e9
Joao Marques <1192221@isep.ipp.pt>
2021-05-19 09:20:24 +0100

    no message

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,9 +14,11 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Joao Miranda - 1200614@isep.ipp.pt \n");
+        System.out.printf("\t João Miranda - 1200614@isep.ipp.pt \n");
         System.out.printf("\t Milene Farias - 1170878@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t João Marques - 1192221@isep.ipp.pt \n");
+        System.out.printf("\t Grigory Gil - 1191902@isep.ipp.pt \n");
+        System.out.printf("\t José Silva - 1190772@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }

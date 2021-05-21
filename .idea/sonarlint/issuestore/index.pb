6719c77cdc0dac74099f8c99d9902ebb96a5ca5f
Joao Marques <1192221@isep.ipp.pt>
2021-05-21 15:30:28 +0100

    no message

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+O
+docs/SprintC/US14/US14_SSD.puml,f/c/fc975f2ac25582a586d61c165934f286b71bde3b
+N
+docs/SprintC/US14/US14_SSD.svg,9/a/9ae189de9b55a2fa2afdd3912a9ca5cbcf55a203
+W
+'src/main/java/app/domain/model/Lab.java,d/a/daf6f4b4de1e5a9b0bf1ecbe913a9918808164f1
+[
++src/test/java/app/domain/model/LabTest.java,9/a/9aa586d56dda01e0cc957829447f49a7f102d334
+I
+docs/SprintC/US14/US14.md,6/f/6f96408c492c73eec23f503267589633c522eb76
---
+++ src/test/java/app/domain/model/LabTest.java
@@ -0,0 +1,52 @@
+package app.domain.model;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LabTest {
+
+    @Test
+    public void getLabId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getAddress() {
+    }
+
+    @Test
+    public void getPhoneNr() {
+    }
+
+    @Test
+    public void getTINnr() {
+    }
+
+    @Test
+    public void setLabId() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void setAddress() {
+    }
+
+    @Test
+    public void setPhoneNr() {
+    }
+
+    @Test
+    public void setTINnr() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}

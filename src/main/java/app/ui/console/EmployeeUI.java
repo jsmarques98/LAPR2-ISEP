4ab765d4287a8e08d720269c6aa38f63bde07f27
milenesofiafarias <1170878@isep.ipp.pt>
2021-05-25 11:53:06 +0100

    Finalização do US7

--- src/main/java/app/ui/console/EmployeeUI.java
+++ src/main/java/app/ui/console/EmployeeUI.java
@@ -6,6 +6,7 @@ import app.domain.model.Company;
 import app.domain.model.Employee;
 import app.domain.model.SpecialistDoctor;
 import app.ui.console.utils.Utils;
+import auth.domain.model.Email;
 
 public class EmployeeUI implements Runnable{
 
@@ -42,32 +43,71 @@ public class EmployeeUI implements Runnable{
             }
         }
 
-
         for(int i=0;i<Company.employeeList.size();i++){
             System.out.println(Company.employeeList.get(i));
         }
     }
 
     public String selectRoleId(){
-        return Utils.readLineFromConsole("Enter Employee's role: ");
+        System.out.println("Select one of the following roles:");
+        for(int i=0;i<Employee.roles.size();i++){
+            System.out.println(Employee.roles.get(i));
+        }
+        String role = Utils.readLineFromConsole("Enter Employee's role: ");
+        if(!Employee.roles.contains(role)){
+            System.out.println("Role not found."+"\n");
+            return selectRoleId();
+        }
+        return role;
     }
+
     public String selectName(){
-        return Utils.readLineFromConsole("Enter Employee's name: ");
+        String name = Utils.readLineFromConsole("Enter Employee's name: ");
+        if (name.length() > 36 || name.length() == 0){
+            System.out.println("Name must have less than 36 chars and can't null.");
+            return selectName();
+        }
+        return name;
     }
     public String selectAddress(){
-        return Utils.readLineFromConsole("Enter Employee's address: ");
-
-    }public String selectPhoneNumber(){
-        return Utils.readLineFromConsole("Enter Employee's phone number: ");
+        String addr = Utils.readLineFromConsole("Enter Employee's address: ");
+        if (addr.length() > 31 || addr.length() == 0){
+            System.out.println("Address must have less than 31 chars and can't null.");
+            return selectAddress();
+        }
+        return addr;
+    }
+    public String selectPhoneNumber(){
+        String phoneNumber = Utils.readLineFromConsole("Enter Employee's phone number: ");
+        if (!(phoneNumber.length() == 11)){
+            System.out.println("Phone Number must have 11 chars.");
+            return selectPhoneNumber();
+        }
+        return phoneNumber;
     }
     public String selectEmail(){
-        return Utils.readLineFromConsole("Enter Employee's email: ");
+        String mail = Utils.readLineFromConsole("Enter Employee's email: ");
+        if(!Email.checkFormat(mail)){
+            System.out.println("Invalid Email.");
+            return selectEmail();
+        }
+        return mail;
     }
     public String selectSocCode(){
-        return Utils.readLineFromConsole("Enter Employee's socCode: ");
+        String socCode = Utils.readLineFromConsole("Enter Employee's socCode: ");
+        if (socCode.length() > 5 || socCode.length() == 0){
+            System.out.println("SocCode must have less than 5 chars and can't null.");
+            return selectSocCode();
+        }
+        return socCode;
     }
     public String selectIndxNumber(){
-        return Utils.readLineFromConsole("Enter Employee's IndxNumber: ");
+        String indexNumb =  Utils.readLineFromConsole("Enter Employee's IndxNumber: ");
+        if (indexNumb.length() > 7 || indexNumb.length() == 0){
+            System.out.println("SocCode must have less than 7 chars and can't null.");
+            return selectIndxNumber();
+        }
+        return indexNumb;
     }
 
 }

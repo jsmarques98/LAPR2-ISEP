c8cec019646edbe034aac6c2be365027e2843289
milenesofiafarias <1170878@isep.ipp.pt>
2021-05-25 22:12:48 +0100

    test 2

--- src/test/java/app/domain/model/EmployeeTest.java
+++ src/test/java/app/domain/model/EmployeeTest.java
@@ -1,4 +1,19 @@
+package app.domain.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeTest {
-  
+
+    @Test
+    void createEmployeeId() {
+        assertEquals(0,0);
+    }
+
+
+    @Test
+    void generatePasswordEmployee() {
+        assertEquals(0,0);
+    }
 }

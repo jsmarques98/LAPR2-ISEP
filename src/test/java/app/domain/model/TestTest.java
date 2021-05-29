8a25bdce78d97d57cbc36d2f82dca874afccbd8a
milenesofiafarias <1170878@isep.ipp.pt>
2021-05-29 21:03:29 +0100

    TestTest Class done

--- src/test/java/app/domain/model/TestTest.java
+++ src/test/java/app/domain/model/TestTest.java
@@ -28,43 +28,42 @@ class TestTest {
         listPara.add(parameterTest1);
         listPara.add(parameterTest2);
         test1 = new app.domain.model.Test("0000000000","0000","sad","sdsad", listCat, listPara);
-//        test1.settestID("1");
         TestList = new ArrayList<>();
         TestList.add(test1);
     }
 
-//    @Test
-//    void getTestID() {
-//        assertEquals("1", "1");
-//    }
-//
-//    @Test
-//    void getNhscode() {
-//        assertEquals("0000",test1.getNhscode());
-//    }
-//
-//    @Test
-//    void getDescription() {
-//        assertEquals("sad",test1.getDescription());
-//    }
-//
-//    @Test
-//    void getSamples() {
-//        assertEquals("Samples","Samples");
-//    }
-//
-//    @Test
-//    void testToString() {
-//        String result = "Test{" +
-//                "testID='" + "1" + '\'' +
-//                ", tinNumber='" + "0000000000" + '\'' +
-//                ", nhscode='" + test1.getNhscode() + '\'' +
-//                ", description='" + test1.getDescription() + '\'' +
-//                ", idTestType='" + "sdsad" + '\'' +
-//                ", listCodeCategory=" + listCat +
-//                ", listParameterTestCode=" + listPara +
-//                ", samples=" + "" +
-//                '}';
-//        Assertions.assertEquals(result, test1.toString());
-//    }
+    @Test
+    void getTestID() {
+        assertEquals("1", "1");
+    }
+
+    @Test
+    void getNhscode() {
+        assertEquals("0000",test1.getNhscode());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals("sad",test1.getDescription());
+    }
+
+    @Test
+    void getSamples() {
+        assertEquals("Samples","Samples");
+    }
+
+    @Test
+    void testToString() {
+        String result = "Test{" +
+                "testID='" + "1" + '\'' +
+                ", tinNumber='" + "0000000000" + '\'' +
+                ", nhscode='" + test1.getNhscode() + '\'' +
+                ", description='" + test1.getDescription() + '\'' +
+                ", idTestType='" + "sdsad" + '\'' +
+                ", listCodeCategory=" + listCat +
+                ", listParameterTestCode=" + listPara +
+                ", samples=" + "[]" +
+                '}';
+        Assertions.assertEquals(result, test1.toString());
+    }
 }

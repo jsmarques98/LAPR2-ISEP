2f56a104d6e5b289366011a87abed5ac6feb29b9
JoaoMaria <1200614@isep.ipp.pt>
2021-05-29 16:05:00 +0100

    created test for the class FileUtil

---
+++ src/test/java/app/domain/model/FileUtilTest.java
@@ -0,0 +1,28 @@
+package app.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilTest {
+
+    @Test
+    void createFile() {
+        FileUtil file = new FileUtil();
+        file.createFile("./results/emailAndSMSMessages.txt");
+        File tmpDir = new File("./results/emailAndSMSMessages.txt");
+        boolean exists = tmpDir.exists();
+        assertEquals(true, exists);
+    }
+
+    @Test
+    void  writeFile() throws IOException {
+        FileUtil file = new FileUtil();
+        file.writeFile("./results/emailAndSMSMessages.txt");
+        BufferedReader objReader = new BufferedReader(new FileReader("./results/emailAndSMSMessages.txt"));
+        assertEquals("Your tests are now available in the app!", objReader.readLine());
+    }
+
+}

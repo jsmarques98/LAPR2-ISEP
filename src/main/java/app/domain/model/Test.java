7156457cdb9893f660a5e4f7b259358ef295e471
Joao Marques <1192221@isep.ipp.pt>
2021-06-01 10:06:08 +0100

    Deleting unnecessary things in Test class

--- src/main/java/app/domain/model/Test.java
+++ src/main/java/app/domain/model/Test.java
@@ -3,9 +3,7 @@ package app.domain.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Test {
 

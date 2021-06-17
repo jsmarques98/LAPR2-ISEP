83fc967d37e3906dae2624564b83c5a5ffa5a54a
1190772 <1190772@isep.ipp.pt>
2021-06-17 17:45:27 +0100

    Alteracao

--- src/main/java/app/ui/console/ViewTestResultsUI.java
+++ src/main/java/app/ui/console/ViewTestResultsUI.java
@@ -1,22 +1,25 @@
 package app.ui.console;
 
 import app.controller.App;
+import app.controller.ViewTestResultsController;
 import app.domain.model.Client;
 import app.domain.model.Company;
 import auth.domain.model.Email;
 
 public class ViewTestResultsUI implements Runnable {
 
+    private final ViewTestResultsController controller;
+
+    public ViewTestResultsUI() {
+        controller = new ViewTestResultsController();
+    }
+
 
     @Override
     public void run() {
         Email email = App.getInstance().getCurrentUserSession().getUserId();
-        Client currentClient = null;
-        for (Client c : Company.clientsList) {
-            if (email.getEmail().equals(c.getEmail())) {
-                currentClient = c;
-            }
-        }
+        System.out.println(cont);
+
 
     }
 }
